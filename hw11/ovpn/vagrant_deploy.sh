3fb5707c21c8f6ae0dd79f8f4049af07f8d64579
Anton Olifir <randolph.o.webb@gmail.com>
2018-06-24 02:39:34 +0300

    deploy update

--- hw11/ovpn/vagrant_deploy.sh
+++ hw11/ovpn/vagrant_deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+mkdir ovpn_test_reddare && cd ovpn_test_reddare
+wget https://raw.githubusercontent.com/reddare/otus-linux/master/hw11/ovpn/Vagrantfile
 vagrant plugin install vagrant-scp
 vagrant up ovpn
 mkdir client
